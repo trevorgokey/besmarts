88ab24d74ee2651d8c6ab4c1f3f5d88138405efd
Trevor Gokey <trevorgokey@gmail.com>
2024-02-12 11:36:23 -0800

    remove mass model from bond model

--- besmarts-core/python/besmarts/mm/force_harmonic.py
+++ besmarts-core/python/besmarts/mm/force_harmonic.py
@@ -40,68 +40,6 @@ def smiles_assignment_energy_function_spring(pos, params):
         ene[ic] = energy_function_spring(k=k, l=l, x=x)
     return ene
 
-def chemical_model_mass_smarts(pcp: mm.perception_model) -> mm.chemical_model:
-    """
-    """
-
-    cm = mm.chemical_model("M", "mass", topology.atom)
-
-    cm.energy_function = None 
-    cm.force_function = None 
-    cm.internal_function = None
-
-    # define the terms of this model
-    cm.topology_terms = {
-        "m": mm.topology_term("m", "mass", "float", "amu", {}, "", {}),
-    }
-
-    ############################################################################
-    # the terms are determined by a smarts matching
-    proc = mm.chemical_model_procedure_smarts_assignment(pcp, cm.topology_terms)
-    proc.name = "Mass assignment"
-    # define the perception model for this force
-
-
-    # we descend into the unit hierarchy when the SMARTS matches at least once,
-    # and I think we only search superSMARTS here and so disable all atomic
-    # primitives
-    # we may want to provide an explicit mapping here
-
-    # proc.unit_hierarchy.smarts[u.index] = "[*:1]"
-
-    # this is the hierarchy within the first main hierarchy node
-    proc.smarts_hierarchies = {
-        0: hierarchies.structure_hierarchy(
-            trees.tree_index(), {}, {}, topology.atom
-        )
-    }
-
-    i = proc.smarts_hierarchies[0].index.node_add_below(None)
-    i.name = "m0"
-    proc.smarts_hierarchies[0].smarts[i.index] = f"[#0:1]"
-
-    # define the parameter order for this force
-    proc.topology_parameters[(0, i.name)] = {"m": i.name}
-
-    # add the term values
-    cm.topology_terms["m"].values[i.name] = [0.0]
-
-    # # create a default param
-    for elem in primitives.element_tr:
-        i = proc.smarts_hierarchies[0].index.node_add_below(None)
-        i.name = "m"+elem
-        proc.smarts_hierarchies[0].smarts[i.index] = f"[#{elem}:1]"
-
-        # define the parameter order for this force
-        proc.topology_parameters[(0, i.name)] = {"m": i.name}
-
-        # add the term values
-        cm.topology_terms["m"].values[i.name] = [float(elem)]
-
-    cm.procedures.append(proc)
-
-    return cm
-
 # chemical models
 def chemical_model_bond_harmonic_smarts(pcp: mm.perception_model) -> mm.chemical_model:
     """
