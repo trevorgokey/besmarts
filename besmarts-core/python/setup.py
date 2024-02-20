64d23d2c198d07afcde40bb6a05d3e4b3aff0b33
Trevor Gokey <trevorgokey@gmail.com>
2024-02-19 21:44:49 -0800

    setup.py fixes

--- besmarts-core/python/setup.py
+++ besmarts-core/python/setup.py
@@ -14,18 +14,19 @@ setuptools.setup(
     url='https://github.com/trevorgokey/besmarts',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    
+    packages=["besmarts"],
     install_requires=requirements,
-    keywords='besmarts',
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    extras_require={"rdkit": ['besmarts-rdkit']}
 )
--- besmarts-rdkit/python/setup.py
+++ besmarts-rdkit/python/setup.py
@@ -1,29 +1,29 @@
 import setuptools
 
-requirements = ["rdkit"]
+requirements = ["besmarts", "rdkit"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='besmarts-rdkit',
-    description="Binary Encoded SMARTS rdkit plugin",
+    description="BESMARTS RDKit plugin",
     license="MIT",
     author="Trevor Gokey",
     author_email='tgokey@uci.edu',
     url='https://github.com/trevorgokey/besmarts',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    
+    packages=["besmarts"],
     install_requires=requirements,
-    keywords='besmarts'
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
