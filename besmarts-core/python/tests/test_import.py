ddb4dcee8891383706c3af3cc86fd083b0e5b496
Trevor Gokey <trevorgokey@gmail.com>
2024-03-16 19:22:16 -0700

    fix import test

--- besmarts-core/python/tests/test_import.py
+++ besmarts-core/python/tests/test_import.py
@@ -4,52 +4,58 @@ besmarts.tests.test_import
 Import all files. Mostly designed as a test to list every object in the codebase
 but can also detect circular imports and check if pypy3 can run everything
 """
-from besmarts.core import (
-    arrays,
-    assignments,
-    chem,
-    clusters,
-    codecs,
-    compute,
-    configs,
-    enumerate,
-    geometry,
-    graph_visitors,
-    graphs,
-    hierarchies,
-    hierarchy_merge,
-    mapper,
-    mm,
-    optimization,
-    primitives,
-    returns,
-    rulesets,
-    splits,
-    topology,
-    tree_iterators,
-    tree_merge,
-    trees,
-)
 
-from besmarts.codecs import (
-    codec_native,
-)
-from besmarts.cluster import (
-    cluster_assignment,
-    cluster_objective,
-    cluster_optimization,
-)
+import unittest
 
-from besmarts.assign import (
-    hierarchy_assign_native,
-)
+class test_import(unittest.TestCase):
+    def test_import_core(self):
+        from besmarts.core import (
+            arrays,
+            assignments,
+            chem,
+            clusters,
+            codecs,
+            compute,
+            configs,
+            geometry,
+            graph_visitors,
+            graphs,
+            hierarchies,
+            hierarchy_merge,
+            mapper,
+            optimization,
+            primitives,
+            returns,
+            splits,
+            topology,
+            tree_iterators,
+            tree_merge,
+            trees,
+        )
 
-from besmarts.mm import (
-    force_harmonic,
-    force_pairwise,
-    force_periodic,
-    masses,
-    smirnoff_models,
-    smirnoff_xml,
-)
+    def test_import_codecs(self):
+        from besmarts.codecs import (
+            codec_native,
+        )
+    def test_import_cluster(self):
+        from besmarts.cluster import (
+            cluster_assignment,
+            cluster_objective,
+            cluster_optimization,
+        )
+
+    def test_import_assign(self):
+        from besmarts.assign import (
+            hierarchy_assign_native,
+        )
+
+    def test_import_mechanics(self):
+        from besmarts.mechanics import (
+            force_harmonic,
+            force_pairwise,
+            force_periodic,
+            masses,
+            smirnoff_models,
+            smirnoff_xml,
+        )
 
