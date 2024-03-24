2a3049168125e6e2a93725d4e7dd00d6d5eb6f88
Trevor Gokey <trevorgokey@gmail.com>
2024-03-23 23:15:25 -0700

    crude ff fit example

--- besmarts-scipy/python/besmarts/mechanics/optimizers_scipy.py
+++ besmarts-scipy/python/besmarts/mechanics/optimizers_scipy.py
@@ -6,6 +6,7 @@ besmarts.mechanics.optimizers_scipy
 import copy
 import scipy.optimize
 
+from besmarts.core import geometry
 from besmarts.mechanics import molecular_models as mm
 from besmarts.mechanics import objectives
 
@@ -34,3 +35,53 @@ def optimize_positions_scipy(csys, psys: mm.physical_system):
         pos.selections[n][c][i] = v
 
     return pos
+
+def ff_obj(x, keys, csys, refpsys, ref, refcsys):
+    # print("input array:")
+    # print(x)
+    pos = copy.deepcopy(ref)
+    recalc = set([x[0] for x in keys])
+
+    reuse = set(range(len(csys.models))).difference(recalc)
+    psys = mm.chemical_system_to_physical_system(csys, pos, ref=refpsys, reuse=reuse)
+    optpos = optimize_positions_scipy(csys, psys)
+
+    for k, v in zip(keys, x):
+        mm.chemical_system_set_value(csys, k, v)
+
+    obj = 0
+    for ic, rxyz in ref[0].selections.items():
+        oxyz = optpos.selections[ic]
+        for a, b in zip(rxyz, oxyz):
+            dx = geometry.array_distance(a, b)
+            dx2 = dx*dx
+            obj += dx2
+            # print(obj, dx2)
+    for k in keys:
+        newv = mm.chemical_system_get_value(csys, k)
+        refv = mm.chemical_system_get_value(refcsys, k)
+        print(k, f"| New: {newv} Ref {refv} Diff {newv-refv}")
+    print("Total obj:", obj)
+    return obj
+
+def optimize_forcefield_scipy(csys, pos):
+    """
+    Take a csys and a psys, then fit the params according to the flattened list
+    The flattened list is the psys values.
+    I need to append all psys values into the same list.
+    Then, I need to take all modified values and reassign to psys
+    """
+    kv = mm.chemical_system_iter_keys(csys)
+    kv = {k:v for k, v in kv.items() if k[0] in [0,1,2] and k[1] in "kl"}
+    keys = list(kv.keys())
+    x0 = list(kv.values())
+    psys = mm.chemical_system_to_physical_system(csys, pos)
+    # x0 = [x*.5 for x in x0]
+
+    result = scipy.optimize.minimize(
+        ff_obj,
+        x0,
+        args=(keys, csys, psys, pos, copy.deepcopy(csys)),
+        options={'disp': False, 'gtol': .1}
+    )
+    return {k:v for k,v in zip(keys, result.x)}
